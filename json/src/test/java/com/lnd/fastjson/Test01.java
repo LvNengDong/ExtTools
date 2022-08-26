d58d1c591318ecbd842ecf2abd8c22f561baf340
nengdong.lv <993696248@qq.com>
2022-08-26 14:29:30 +0800

    fastJson格式化json字符串

--- json/src/test/java/com/lnd/fastjson/Test01.java
+++ json/src/test/java/com/lnd/fastjson/Test01.java
@@ -18,8 +18,8 @@ public class Test01 {
             >> 无格式的 json 字符串
             >> 转换成一个 JSONObject
             >> JSONObject 可以转换成
-                >> 1、未格式化的json字符串
-                >> 2、格式化的json字符串
+                >> 1、普通格式的json字符串
+                >> 2、漂亮格式的json字符串
         */
         String jsonString = "{\"_index\":\"book_shop\",\"_type\":\"it_book\",\"_source\":{\"date\":\"2007-06-01\",\"author\":\"[美] Bruce Eckel\",\"price\":109.0,\"name\":\"Java编程思想（第4版）\",\"publisher\":\"机械工业出版社\",\"category\":\"编程语言\",\"tags\":[\"Java\",\"编程语言\"]},\"_id\":\"1\",\"_score\":1.0}";
         JSONObject object = JSON.parseObject(jsonString);
@@ -30,7 +30,35 @@ public class Test01 {
 
         System.out.println("===========================");
 
-        String format = JSON.toJSONString(object, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue, SerializerFeature.WriteDateUseDateFormat);
+        String format = JSON.toJSONString(object, SerializerFeature.PrettyFormat); //漂亮的格式
         System.out.println(format); // 格式化后的json字符串
     }
+
+    /* 执行以上代码，输出结果为：
+    --------------------------------------
+    {"_index":"book_shop","_type":"it_book","_source":{"date":"2007-06-01","author":"[美] Bruce Eckel","price":109.0,"name":"Java编程思想（第4版）","publisher":"机械工业出版社","category":"编程语言","tags":["Java","编程语言"]},"_id":"1","_score":1.0}
+
+    ===========================
+
+    {
+        "_index":"book_shop",
+        "_type":"it_book",
+        "_source":{
+            "date":"2007-06-01",
+            "author":"[美] Bruce Eckel",
+            "price":109.0,
+            "name":"Java编程思想（第4版）",
+            "publisher":"机械工业出版社",
+            "category":"编程语言",
+            "tags":[
+                "Java",
+                "编程语言"
+            ]
+        },
+        "_id":"1",
+        "_score":1.0
+    }
+    -----------------------------------
+    分析：
+    */
 }
